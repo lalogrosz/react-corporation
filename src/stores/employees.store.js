c6fd4379585f4d160f6ab8c968ff2cd9be3a7ed1
Alan Grosz <lalogrosz@gmail.com>
2020-06-06 17:03:58 -0300

    Readme and title changes. Remove unused import

--- src/stores/employees.store.js
+++ src/stores/employees.store.js
@@ -1,4 +1,4 @@
-import { observable, action, decorate } from "mobx";
+import { observable, decorate } from "mobx";
 import employeeService from "../services/employee.service";
 
 class EmployeesStore {
